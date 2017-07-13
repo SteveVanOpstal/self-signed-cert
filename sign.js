3872035bd0a551142ecabb12760d8201f3d821d6
Steve Van Opstal <SteveVanOpstal@users.noreply.github.com>
2017-07-13 10:14:40 +0200

    feat: add pem

--- sign.js
+++ sign.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
-const selfsigned = require("selfsigned");
-const attrs = [{ name: "commonName", value: "localhost" }];
-const pems = selfsigned.generate(attrs, {
-    algorithm: "sha256",
-    keySize: 2048,
-    extensions: [{
-        name: "subjectAltName",
-        altNames: [{
-            type: 2, // DNS
-            value: "localhost"
-        }]
+const fs = require('fs');
+const selfsigned = require('selfsigned');
+const pems = selfsigned.generate([{name: 'commonName', value: 'localhost'}], {
+  algorithm: 'sha256',
+  keySize: 2048,
+  extensions: [{
+    name: 'subjectAltName',
+    altNames: [{
+      type: 2,  // DNS
+      value: 'localhost'
     }]
+  }]
 });
 
-fs.writeFileSync("./server.crt", pems.cert, { encoding: "utf-8" });
-fs.writeFileSync("./server.key", pems.private, { encoding: "utf-8" });
+fs.writeFileSync('./cert.crt', pems.cert, {encoding: 'utf-8'});
+fs.writeFileSync('./cert.key', pems.private, {encoding: 'utf-8'});
+fs.writeFileSync('./cert.pem', pems.private + pems.cert, {encoding: 'utf-8'});
